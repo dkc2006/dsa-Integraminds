f0bddf2002c5a0ae136613834c2411eccb92f09b
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-01-12 23:35:30 +0530

    Data Types

--- src/main/java/day_1_Basics_Of_Java/DataTypes.java
+++ src/main/java/day_1_Basics_Of_Java/DataTypes.java
@@ -1,4 +1,12 @@
 package day_1_Basics_Of_Java;
 
 public class DataTypes {
+    public static void main(String[] args) {
+        long x=2147483648L;
+        System.out.println(x);
+
+        double l=887.8;
+        float f=67.65f;
+    }
+
 }
