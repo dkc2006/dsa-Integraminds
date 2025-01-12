c9577579295c3026d315a557500c363a4bb4ef5b
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-01-12 23:25:35 +0530

    functions

---
+++ src/main/java/day_1_Basics_Of_Java/Functions.java
@@ -0,0 +1,18 @@
+package day_1_Basics_Of_Java;
+
+public class Functions {
+    //code reusability
+    static void address()
+    {
+        System.out.println("Rakesh");
+        System.out.println("No 32");
+        System.out.println("Abc Street");
+        System.out.println("Chennai");
+        System.out.println("------------");
+    }
+
+    public static void main(String[] args) {
+        address();
+        address();
+    }
+}
