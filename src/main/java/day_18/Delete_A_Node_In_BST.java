945a9db5a8d4b6ca222a2ce0ca13afdeec24aac5
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2025-03-29 13:42:34 +0530

    Day-18 class codes [Binary Search Tree (Questions)]

---
+++ src/main/java/day_18/Delete_A_Node_In_BST.java
@@ -0,0 +1,53 @@
+package day_18;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+public class Delete_A_Node_In_BST {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            TreeNode minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(7);
+
+        Delete_A_Node_In_BST deleter = new Delete_A_Node_In_BST();
+        int key = 3;
+        root = deleter.deleteNode(root, key);
+
+        System.out.println("Node with value " + key + " deleted from BST.");
+    }
+}
